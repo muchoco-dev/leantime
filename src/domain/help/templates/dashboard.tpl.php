99922fa10d35681b721cb7ddaf912b7609172c17
aya <aya.yoshioka.1116@gmail.com>
2020-09-09 14:22:59 +0900

    チュートリアルのトップ変更

--- src/domain/help/templates/dashboard.tpl.php
+++ src/domain/help/templates/dashboard.tpl.php
@@ -16,32 +16,32 @@
     <div class="row onboarding">
         <div class="col-md-4">
             <div style='width:50%' class='svgContainer'>
-                <?php    echo file_get_contents(ROOT."/images/svg/undraw_design_data_khdb.svg");
-                echo"</div>";?>
-
-                <br />
-            <h4 class="primaryColor"><?php echo $this->__('headlines.step_one_discover') ?></h4>
-
-            <p><?php echo $this->__('text.business_research_section') ?></p>
-
-        </div>
-        <div class="col-md-4">
-            <div style='width:50%' class='svgContainer'>
                 <?php    echo file_get_contents(ROOT."/images/svg/undraw_scrum_board_cesn.svg");
                 echo"</div>";?>
-                <br />
-            <h4 class="primaryColor"><?php echo $this->__('headlines.step_two_plan') ?></h4>
 
-            <p><?php echo $this->__('text.our_roadmap_design') ?></p>
+                <br />
+            <h4 class="primaryColor">1. タスク管理</h4>
+
+            <p>タスク管理ページでは、このプロジェクト全体のタスク(TODO)を管理しています。手が空いているときは積極的に未着手のタスクを解決しましょう。</p>
+
         </div>
         <div class="col-md-4">
             <div style='width:50%' class='svgContainer'>
                 <?php    echo file_get_contents(ROOT."/images/svg/undraw_time_management_30iu.svg");
                 echo"</div>";?>
                 <br />
-            <h4 class="primaryColor"><?php echo $this->__('headlines.step_three_track') ?></h4>
+            <h4 class="primaryColor">2. 作業時間管理</h4>
 
-            <p><?php echo $this->__('text.this_is_where_you_spend_most_time') ?></p>
+            <p>タイムシートを使って、作業時間の記録を忘れないようにしてください。その記録を元に計画や報酬の計算、契約内容の判断を行っています。</p>
+        </div>
+        <div class="col-md-4">
+            <div style='width:50%' class='svgContainer'>
+                <?php    echo file_get_contents(ROOT."/images/svg/undraw_design_data_khdb.svg");
+                echo"</div>";?>
+                <br />
+            <h4 class="primaryColor">3. 請求管理</h4>
+
+            <p>月が変わったら、前月分の報酬額を計算してご請求ください。承認されると翌月10日までにご指定の口座にお振込みさせていただきます。</p>
         </div>
     </div>
 
